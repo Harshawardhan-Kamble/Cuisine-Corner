d63f0902aea51a40e6d249eed9da3b0a6fb741db
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-11-08 16:16:18 +0530

    res-container styled

--- Cuisine corner/src/Components/RestaurantCard.js
+++ Cuisine corner/src/Components/RestaurantCard.js
@@ -4,9 +4,9 @@ const RestuarantCard = ({ name, cuisine, rating, price, delivery, img }) => {
       <img className="res-logo" src={img} alt="" />
       <h3>{name}</h3>
       <p>{cuisine.join(",")}</p>
-      <span>&#9733;{rating}</span>
-      <span>&#x2022;{price}</span>
-      <span>&#x2022;{delivery}</span>
+      <span>&#9733; {rating}</span>
+      <span>&#x2022; {price}</span>
+      <span>&#x2022; {delivery}</span>
     </div>
   );
 };
