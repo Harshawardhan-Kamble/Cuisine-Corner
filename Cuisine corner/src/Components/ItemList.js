4da8f30214da00f573eec0c5b2adfa1a9e6e6bd7
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-11-09 15:50:55 +0530

    add button styled

--- Cuisine corner/src/Components/ItemList.js
+++ Cuisine corner/src/Components/ItemList.js
@@ -1,19 +1,16 @@
 import { useDispatch } from "react-redux";
 import { CDN_IMAGE } from "../utils/constant";
 import { addItem } from "../utils/redux/cartSlice";
-const ItemList = ({ items}) => {
+const ItemList = ({ items }) => {
   // console.log(items);
-  const dispatch=useDispatch()
-  const handleAddItem=(item)=>{
-    dispatch(addItem(item))
-  }
+  const dispatch = useDispatch();
+  const handleAddItem = (item) => {
+    dispatch(addItem(item));
+  };
   return (
     <div>
       {items.map((item) => (
-        <div
-          className="menu-item-container"
-          key={item?.card?.info?.id}
-        >
+        <div className="menu-item-container" key={item?.card?.info?.id}>
           <div className="menu-item-card">
             <p>{item?.card?.info?.name}</p>
             <p>
@@ -21,22 +18,25 @@ const ItemList = ({ items}) => {
               {Math.round(item.card.info.price / 100) ||
                 item.card.info.defaultPrice / 100}
             </p>
-            <p>
-              {item?.card?.info?.description}
-            </p>
+            <p>{item?.card?.info?.description}</p>
           </div>
-              <div className="item-add">
-                <button onClick={()=>handleAddItem(item)} className="item-add-btn">
-                  Add
-                </button>
-              </div>
-          {item?.card?.info?.imageId ? (
-            <div className="item-img">
-              <img
-                src={CDN_IMAGE + item?.card?.info?.imageId}
-              />
+
+          {item?.card?.info?.imageId && (
+            <div
+              className="item-img"
+              style={{ display: item?.card?.info?.imageId ? "block" : "none" }}
+            >
+              <img src={CDN_IMAGE + item?.card?.info?.imageId} />
             </div>
-          ) : null}
+          )}
+          <div className="item-add">
+            <button
+              onClick={() => handleAddItem(item)}
+              className="item-add-btn"
+            >
+              Add
+            </button>
+          </div>
 
           {/* <div className="w-3/12 p-4"><img src={CDN_IMAGE+item?.card?.info?.imageId} className="w-full"/></div> */}
         </div>
