f44be9b17e0471c52a9cf9918a292a8a5916b4f0
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-11-09 17:31:39 +0530

    empty cart styled

--- Cuisine corner/src/Components/Cart.js
+++ Cuisine corner/src/Components/Cart.js
@@ -1,26 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../utils/redux/cartSlice";
 import ItemList from "./ItemList";
+import emptycart from "../../assets/emptycart.jpg";
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  const dispatch=useDispatch()
-  const handleClearCart=()=>{
-    dispatch(clearCart())
-  }
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
   return (
     <>
       <div className="text-center font-bold mt-10 ">
-        <p className="font-bold">Cart</p>
+        {/* <p className="font-bold">Cart</p> */}
       </div>
 
       {cartItems.length === 0 ? (
-        <h1 className="text-center font-bold ">
-          Cart empty. Add something to cart
-        </h1>
+        <div className="empty-cart">
+          <h1>
+            Your cart is lonely. Fill it up with some of our delicious dishes
+          </h1>
+          <img className="empty-cart-img" src={emptycart}></img>
+        </div>
       ) : (
         <>
           <div className="text-center my-4">
-            <button onClick={handleClearCart} className="p-2 font-bold m-auto bg-gray-400 rounded-md ">
+            <button
+              onClick={handleClearCart}
+              className="p-2 font-bold m-auto bg-gray-400 rounded-md "
+            >
               Clear Cart
             </button>
           </div>
