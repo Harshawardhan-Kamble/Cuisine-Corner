68d37b0a79b64addc798d3a39bafb135dbe7ab44
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-11-12 08:51:57 +0530

    cart styled

--- Cuisine corner/src/Components/Cart.js
+++ Cuisine corner/src/Components/Cart.js
@@ -10,41 +10,45 @@ const Cart = () => {
     dispatch(clearCart());
   };
   const calculateCartTotal = () => {
-    return cartItems.reduce((total, item) =>  total + Math.round(item.card.info.price/100), 0);
+    return cartItems.reduce(
+      (total, item) => total + Math.round(item.card.info.price / 100),
+      0
+    );
   };
   return (
-    <div className="cart-container">
-
+    <>
       {cartItems.length === 0 ? (
         <div className="empty-cart">
           <h1>
-            Your cart is lonely. Fill it up with some of our delicious dishes <span><Link to="/">&#8594;</Link></span></h1>
+            Your cart is lonely. Fill it up with some of our delicious dishes{" "}
+            <span>
+              <Link to="/">&#8594;</Link>
+            </span>
+          </h1>
           <img className="empty-cart-img" src={emptycart}></img>
         </div>
       ) : (
         <>
-      <div className="container-1">  
-      <div className="text-center font-bold mt-10 ">
-        <p className="font-bold">Cart Page</p>
-      </div>
           <div className="text-center my-4">
-            <button
-              onClick={handleClearCart}
-              className="clear-cart-btn" 
-            >
+            <button onClick={handleClearCart} className="clear-cart-btn">
               Clear Cart
             </button>
           </div>
-          <div className="w-6/12 m-auto">
-            <ItemList items={cartItems} />
-          </div>
-          </div>
-          <div className="total-cart-price">
-            <p>Total Cart Price: ₹{calculateCartTotal()}</p>
+          <div className="cart-container">
+            <div className="cart-items">
+            <p>Cart Page</p>
+              <ItemList items={cartItems} />
+            </div>
+            <div className="total-cart-price">
+              <p>Sub-Total: ₹{calculateCartTotal()}</p>
+              <p>Delivery Charges: ₹100</p>
+              <p>Grand-Total: ₹{calculateCartTotal() +100}</p>
+              <button className="cart-checkout">Checkout</button>
+            </div>
           </div>
         </>
       )}
-    </div>
+    </>
   );
 };
 export default Cart;
