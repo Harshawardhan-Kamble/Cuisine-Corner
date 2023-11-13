c213fa18b96d10c958ac8e1d57aaaaed7bdcd5eb
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-11-13 21:24:16 +0530

    about us

--- Cuisine corner/src/Components/About.js
+++ Cuisine corner/src/Components/About.js
@@ -1,4 +1,31 @@
+import aboutUsImage from "../../assets/aboutus.png";
 const About = () => {
-  return <h1>Jan Ke kya he karega</h1>;
+  return (
+    <>
+      <div className="about-us">
+        <div className="about-us-content">
+          <h1>About us</h1>
+          <p>
+            Welcome to Cuisine Corner, the Premier Food Ordering app in Pune,
+            Maharashtra. We are dedicated to providing a seamless and convenient
+            experience for food lovers to explore and order from a wide range of
+            local restaurants.
+          </p>
+          <br></br>
+          <p>
+            At Cuisine Corner, we understand the importance of good food and the
+            joy it brings. Our platform is designed to connect hungry customers
+            with their favorite cuisines, enabling them to satisfy their
+            cravings with just a few taps on their mobile devices. With a
+            user-friendly interface and a vast selection of restaurants, we
+            strive to make every meal a delightful and hassle-free experience.
+          </p>
+        </div>
+        <div>
+          <img className="about-us-image" src={aboutUsImage}></img>
+        </div>
+      </div>
+    </>
+  );
 };
 export default About;
