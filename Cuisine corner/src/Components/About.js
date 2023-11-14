52c5c11c1140d8aea8d39c4037e0a811344df8f1
harsh <kambleharsh08@outlook.com>
2023-11-15 00:56:18 +0530

    about us

--- Cuisine corner/src/Components/About.js
+++ Cuisine corner/src/Components/About.js
@@ -1,4 +1,5 @@
 import aboutUsImage from "../../assets/aboutus.png";
+import profilepic from "../../assets/profilepic.jpg";
 const About = () => {
   return (
     <>
@@ -6,10 +7,10 @@ const About = () => {
         <div className="about-us-content">
           <h1>About us</h1>
           <p>
-            Welcome to Cuisine Corner, the Premier Food Ordering Platform in Pune,
-            Maharashtra. We are dedicated to providing a seamless and convenient
-            experience for food lovers to explore and order from a wide range of
-            local restaurants.
+            Welcome to Cuisine Corner, the Premier Food Ordering Platform in
+            Pune, Maharashtra. We are dedicated to providing a seamless and
+            convenient experience for food lovers to explore and order from a
+            wide range of local restaurants.
           </p>
           <br></br>
           <p>
@@ -23,6 +24,27 @@ const About = () => {
         </div>
         <div>
           <img className="about-us-image" src={aboutUsImage}></img>
+        </div>
+      </div>
+      <div className="founder-section">
+        <div>
+          <img className="founder-image" src={profilepic}></img>
+        </div>
+        <div className="founder-words" >
+          <h2>Our Mission</h2>
+          <p className="founder-text">
+            At Cuisine Corner, our vision is to create a world where food
+            becomes a gateway to joy, discovery, and connection. We strive to be
+            the go-to platform that seamlessly introduces individuals to the
+            diverse tapestry of global cuisines. Our mission is rooted in
+            redefining the dining experience – making it not just a matter of
+            convenience but a journey filled with excitement, innovation, and a
+            celebration of flavors. Through our commitment to culinary
+            diversity, seamless experiences, and empowering local businesses, we
+            aspire to make every meal an exploration and every bite a moment of
+            pure delight. Cuisine Corner isn't just about ordering food; it's
+            about creating culinary connections that leave lasting impressions.
+          </p>
         </div>
       </div>
     </>
