1643453a8367c179023d268b16fe285279d380ee
harsh <kambleharsh08@outlook.com>
2023-11-15 01:06:31 +0530

    footer

--- Cuisine corner/src/Components/Footer.js
+++ Cuisine corner/src/Components/Footer.js
@@ -1,4 +1,23 @@
+// const Footer = () => {
+//   return <div className="footer">&copy;Cuisines Corner</div>;
+// };
+import React from 'react';
+
 const Footer = () => {
-  return <div className="footer">&copy;Cuisines Corner</div>;
+  return (
+    <footer className="app-footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Cuisine Corner. All rights reserved.</p>
+        <div className="social-links">
+          <a href="#" target="_blank" rel="noopener noreferrer">Github</a>
+          <a href="#" target="_blank" rel="noopener noreferrer">Linkedin</a>
+          <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
+        </div>
+      </div>
+    </footer>
+  );
 };
+
+// export default Footer;
+
 export default Footer;
