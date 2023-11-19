fec823e6b97dc0bb943dea8f0e9810fcba3b0cc1
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-11-19 08:42:55 +0530

    api issue solved

--- Cuisine corner/src/Components/RestaurantList.js
+++ Cuisine corner/src/Components/RestaurantList.js
@@ -22,7 +22,7 @@ const RestaurantList = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     setResList(
