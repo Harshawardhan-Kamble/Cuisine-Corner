2a220bd29888511977c10897dc7e65e80bb30869
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-12-20 19:54:05 +0530

    api cors issued resolved

--- Cuisine corner/src/Components/RestaurantList.js
+++ Cuisine corner/src/Components/RestaurantList.js
@@ -22,7 +22,7 @@ const RestaurantList = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D18.5204303%26lng%3D73.8567437%26page_type%3DDESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     let list =
