f8d3fa8c5d8789aaa735e1780407470b6b2ab963
Harshawardhan Kamble <kambleharsh08@outlook.com>
2024-02-08 15:05:28 +0530

    Cancel page styled

--- Cuisine corner/src/Components/Cancel.js
+++ Cuisine corner/src/Components/Cancel.js
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 const Cancel = () => {
   return (
-    <div>Cancel</div>
+    <div className='payment-failure'>
+
+      <div class="payment-failure-container">
+        
+        <div class="payment-failure-message">
+          <p><strong>Oops! Payment Failed</strong></p>
+          <p>We're sorry, but we couldn't process your payment.</p>
+        </div>
+        {/* <div class="payment-failure-suggestions">
+          <p>Here are a few suggestions:</p>
+          <ul>
+            <li>Double-check your payment information for accuracy.</li>
+            <li>Ensure your account has sufficient funds or try another payment method.</li>
+            <li>If the issue persists, contact your bank or financial institution.</li>
+          </ul>
+        </div> */}
+        <div class="payment-failure-contact">
+            
+            
+              <Link to="/contactus">
+                
+          <button>Contact Support Team
+            
+          </button>
+              </Link>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
-export default Cancel
+export default Cancel  
