7573ec3aaa85b7880df3f429af683dcb63896187
harsh <kambleharsh08@outlook.com>
2025-04-14 01:13:53 +0530

    removed stripe

--- backend/app.js
+++ backend/app.js
@@ -42,29 +42,6 @@ app.get('/menu/api/:resId',async(req,res)=>{
   }
   
 })
-app.post("/create-checkout-session", async (req, res) => {
-  const { products } = req.body;
-  // console.log(products);
-  const lineItems = products.map((product) => ({
-    price_data: {
-      currency: "inr",
-      product_data: {
-        name: product?.card?.info?.name,
-      },
-      unit_amount:Math.round(product?.card?.info?.price ) || Math.round(product?.card?.info?.defaultPrice )
-    },
-    quantity: 1,
-  }));
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: lineItems,
-    mode: "payment",
-    success_url: "http://localhost:1234/success",
-    cancel_url: "http://localhost:1234/cancel",
-  });
-  res.json({ id: session.id });
-});
 app.listen(3000, () => {
-  console.log("Server Started");
+  console.log("Server Started on port 3000");
 });
